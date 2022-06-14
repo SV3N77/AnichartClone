a443820ed0d592a1d6c44bcb2a161cb1b661a95f
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2022-06-14 19:23:21 +1000

    Updated server to use Handlebars js

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
+const fs = require("fs");
+
+const data = JSON.parse(fs.readFileSync("./public/animecards.json"));
+const animes = data.Animecards;
+
+app.set("view engine", "hbs");
+app.get("/winter", (req, res) => {
+  res.render("index", { animecards: animes });
+});
 
 app.use(express.static("./public"));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(3000, () => console.log("Server started on port 3000"));
