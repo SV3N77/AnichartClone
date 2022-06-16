8448299b3a6451756cf9af972f4835439f2a37f9
SV3N77 <50192528+SV3N77@users.noreply.github.com>
2022-06-16 20:25:17 +1000

    updated and improved styling of media queries

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const data = JSON.parse(fs.readFileSync("./public/animecards.json"));
 const animes = data.Animecards;
 
 app.set("view engine", "hbs");
-app.get("/winter", (req, res) => {
+app.get("/", (req, res) => {
   res.render("index", { animecards: animes });
 });
 
